a88493bb82bf22f74c9f7004ce9fd11476d12d1d
AgustinaHeredia <cuadernomenstrual@gmail.com>
2024-05-27 16:14:33 +0200

    ahora si para entregar

--- src/check_map.c
+++ src/check_map.c
@@ -3,34 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   check_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: agusheredia <agusheredia@student.42.fr>    +#+  +:+       +#+        */
+/*   By: agheredi <agheredi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/02 17:39:43 by agheredi          #+#    #+#             */
-/*   Updated: 2024/05/25 19:58:00 by agusheredia      ###   ########.fr       */
+/*   Updated: 2024/05/27 16:12:54 by agheredi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-int	all_char_valid(t_map *map)
+int	all_char_valid(char **map_game)
 {
 	int	row;
 	int	col;
 
 	row = 0;
-	while (map->map_game[row])
+	while (map_game[row])
 	{
 		col = 0;
-		while (map->map_game[row][col] && map->map_game[row][col] != '\n')
+		while (map_game[row][col] && map_game[row][col] != '\n')
 		{
-			if (map->map_game[row][col] != ' '
-				&& map->map_game[row][col] != '\t'
-				&& map->map_game[row][col] != 'N'
-				&& map->map_game[row][col] != 'S'
-				&& map->map_game[row][col] != 'W'
-				&& map->map_game[row][col] != 'E'
-				&& map->map_game[row][col] != '0'
-				&& map->map_game[row][col] != '1')
+			if (map_game[row][col] != ' '
+				&& map_game[row][col] != '\t'
+				&& map_game[row][col] != 'N'
+				&& map_game[row][col] != 'S'
+				&& map_game[row][col] != 'W'
+				&& map_game[row][col] != 'E'
+				&& map_game[row][col] != '0'
+				&& map_game[row][col] != '1')
 			{
 				return (-1);
 			}
@@ -89,7 +89,7 @@ int	one_player(t_map *map)
 
 void	check_map(t_game *game)
 {
-	if (all_char_valid(&game->map) != 0)
+	if (all_char_valid(game->map.map_game) != 0)
 		error_free_exit(game, "Error. Map has invalid char\n");
 	if (one_player(&game->map) != 0)
 		error_free_exit(game, "Error. Number of player invalid\n");
